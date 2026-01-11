643949fe0ef1b571844342a2c5c76f8b2bc72a0c
MUBEEN SHAIKH <shaikhmomubeenalam@gmail.com>
2026-01-12 01:46:12 +0530

    Fix formatting in BookingConfig class definition

--- msconstruction/booking/apps.py
+++ msconstruction/booking/apps.py
@@ -4,7 +4,6 @@ from django.apps import AppConfig
 class BookingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'booking'class BookingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'msconstruction.booking'
 
 
