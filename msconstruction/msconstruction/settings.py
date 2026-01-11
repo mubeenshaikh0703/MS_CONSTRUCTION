e92039646ade0c91bf64ebd84292c76173cdba67
MUBEEN SHAIKH <shaikhmomubeenalam@gmail.com>
2026-01-12 00:26:05 +0530

    Use environment variables for email credentials
    Replaced hardcoded email credentials with environment variables.

--- msconstruction/msconstruction/settings.py
+++ msconstruction/msconstruction/settings.py
@@ -98,7 +98,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'msroyal0703@gmail.com'   # YOUR REAL GMAIL
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = 'MS Construction <msroyal0703@gmail.com>'
