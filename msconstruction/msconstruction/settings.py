3218714584ff6dcc6c51e3c7294039917720ca1c
MUBEEN SHAIKH <shaikhmomubeenalam@gmail.com>
2026-01-12 10:15:21 +0530

    Fix string quotes for DEFAULT_FROM_EMAIL

--- msconstruction/msconstruction/settings.py
+++ msconstruction/msconstruction/settings.py
@@ -102,4 +102,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = 'MS Construction <msroyal0703@gmail.com>'
+DEFAULT_FROM_EMAIL = "MS Construction <msroyal0703@gmail.com>"
